87f55fcf0a2fd11abe939ceb13000bde136e24ac
Jhonatan Felix <jhonatanvfelix@gmail.com>
2024-04-27 22:40:55 -0300

    Iniciando a implementação das classes Tabuleiro e Peça

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ import tabuleiro.Posicao;
 public class Program {
     public static void main(String[] args) {
         
-        Posicao pos = new Posicao(3, 5);
-        System.out.println(pos);
+        Posicao board  = new Posicao(8,8);
     }
 }
