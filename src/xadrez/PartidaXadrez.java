b24bf287e3f5f887499f2d1f93b65eb5ea61abe0
Jhonatan Felix <jhonatanvfelix@gmail.com>
2024-04-28 22:33:24 -0300

    Criação das classes XadrezException e PosiçãoXadrez

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -1,6 +1,5 @@
 package xadrez;
 
-import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
@@ -24,10 +23,13 @@ public class PartidaXadrez {
         return mat;
     }
 
+    private void colocarNovaPeca(char coluna, int linha, PecaXadrez peca) {
+        tabuleiro.colocarPeca(peca, new XadrezPosicao(coluna, linha).toPosicao());
+    }
     public void iniciarPartida() {
-        tabuleiro.colocarPeca(new Torre(tabuleiro, Color.WHITE), new Posicao(2,1));
-        tabuleiro.colocarPeca(new Rei(tabuleiro, Color.BLACK), new Posicao(0, 4));
-        tabuleiro.colocarPeca(new Rei(tabuleiro, Color.WHITE), new Posicao(7, 4));
+        colocarNovaPeca('b', 6, new Torre(tabuleiro, Color.WHITE));
+        colocarNovaPeca('e', 8, new Rei(tabuleiro, Color.BLACK));
+        colocarNovaPeca('e', 1, new Rei(tabuleiro, Color.WHITE));
     }
 
     
---
+++ src/xadrez/XadrezPosicao.java
@@ -0,0 +1,44 @@
+package xadrez;
+
+import tabuleiro.Posicao;
+
+public class XadrezPosicao {
+
+    private char coluna;;
+    private int linha;
+    
+    public XadrezPosicao(char coluna, int linha) {
+        if(coluna < 'a' || coluna > 'h' || linha < 1 || linha > 8){
+            throw new XadrezException("Erro ao instanciar a posicao. Valores validos são de A1 ate H8");
+        }
+        this.coluna = coluna;
+        this.linha = linha;
+    }
+
+    public char getColuna() {
+        return coluna;
+    }
+
+    
+    public int getLinha() {
+        return linha;
+    }
+
+    protected Posicao toPosicao(){
+        return new Posicao(8 - linha, coluna - 'a');
+    }
+
+    protected static XadrezPosicao fromPosicao(Posicao posicao){
+        return new XadrezPosicao((char)('a' - posicao.getColuna()), 8 - posicao.getLinha());
+    }
+    
+    @Override   
+    public String toString(){
+        return "" + coluna + linha;
+    }
+
+    
+
+    
+
+}
