49cc82d9d56cab922e99d34f40b6abd0535a205e
Jhonatan Felix <jhonatanvfelix@gmail.com>
2024-04-28 22:53:22 -0300

    Implementando pequenas alterações na impresão do tabuleiro

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -27,9 +27,19 @@ public class PartidaXadrez {
         tabuleiro.colocarPeca(peca, new XadrezPosicao(coluna, linha).toPosicao());
     }
     public void iniciarPartida() {
-        colocarNovaPeca('b', 6, new Torre(tabuleiro, Color.WHITE));
-        colocarNovaPeca('e', 8, new Rei(tabuleiro, Color.BLACK));
-        colocarNovaPeca('e', 1, new Rei(tabuleiro, Color.WHITE));
+        colocarNovaPeca('c', 1, new Torre(tabuleiro, Color.WHITE));
+        colocarNovaPeca('c', 2, new Torre(tabuleiro, Color.WHITE));
+        colocarNovaPeca('d', 2, new Torre(tabuleiro, Color.WHITE));
+        colocarNovaPeca('e', 2, new Torre(tabuleiro, Color.WHITE));
+        colocarNovaPeca('e', 1, new Torre(tabuleiro, Color.WHITE));
+        colocarNovaPeca('d', 1, new Rei(tabuleiro, Color.WHITE));
+
+        colocarNovaPeca('c', 7, new Torre(tabuleiro, Color.BLACK));
+        colocarNovaPeca('c', 8, new Torre(tabuleiro, Color.BLACK));
+        colocarNovaPeca('d', 7, new Torre(tabuleiro, Color.BLACK));
+        colocarNovaPeca('e', 7, new Torre(tabuleiro, Color.BLACK));
+        colocarNovaPeca('e', 8, new Torre(tabuleiro, Color.BLACK));
+        colocarNovaPeca('d', 8, new Rei(tabuleiro, Color.BLACK));
     }
 
     
